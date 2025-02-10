170744983067d8e3c063a9c90251a8484cad4847
Suraj-729 <surajkumarsahoo729@gmail.com>
2025-02-10 13:41:18 +0530

    Refactor server setup: remove Server.js and integrate HTTPS server into app.js

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ const connectToDb = require('./Db/db')
 const userRoutes = require('./routes/user.routes');
 
 connectToDb();
-
+const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use(cookieparser());
-
+app.listen(port, () => {
+  console.log(`HTTPS Server is running on port ${port}`);
+});
 app.use('/user', userRoutes);
 // app.use('/images', imageRoutes);
 app.get('/',(req, res) => {

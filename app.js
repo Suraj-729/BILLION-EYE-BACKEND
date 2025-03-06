113267260e8ee36fda16eaff7b8a1114fd9eeadc
Suraj-729 <surajkumarsahoo729@gmail.com>
2025-03-06 11:45:48 +0530

    Add body-parser middleware for improved request handling and update dependencies

--- app.js
+++ app.js
@@ -6,14 +6,23 @@ const cors = require('cors');
 const connectToDb = require('./Db/db')
 const userRoutes = require('./routes/user.routes');
 const agencyRoutes = require('./routes/agency.router');
+// Middleware (order matters!)
+const bodyParser = require('body-parser');
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 connectToDb();
 const port = process.env.PORT || 5000;
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 
-app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  console.log('Request content length:', req.headers['content-length']);
+  next();
+});
+
 // app.use(cookieparser());
 app.listen(port, () => {
   console.log(`HTTPS Server is running on port ${port}`);
